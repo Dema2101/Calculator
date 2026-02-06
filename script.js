913718b197fae2a9b11fa37b363c021ef84c3e33
Marko <202842510+Dema2101@users.noreply.github.com>
2026-02-06 22:46:17 +0100

    Zavšrena implementacija i logika svega, dovršiti još clear button i objaviti na github

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const textSpan = document.querySelector("#year .copyright-text");
 textSpan.textContent = `Copyright © ${new Date().getFullYear()} dema2101`;
 
 
-const screen = document.querySelector(".screen");
+const screen = document.querySelector(".mainScreen");
+const smallScreen = document.querySelector(".smallScreen");
 let number1 = "";
 let operator = "";
 let number2 = "";
 
-
-
 operator=getOperator(screen);
 getNumbers(screen);
+getEqual();
+deleteNumber(screen);
+
+
+
+
+
+
+
+
+
 
 function getNumbers (screen){
 const numbers = document.querySelectorAll(".numbers");
 numbers.forEach(btn => {
     btn.addEventListener("click", ()=> {
+
+    const check =btn.textContent;
+    if(check==="."){
+
+        if(operator ==="" && number1.includes(".")) return;
+        if(operator !=="" && number2.includes(".")) return;
+    }
+
 
 if(operator===""){
 
@@ -35,52 +53,109 @@ if(operator===""){
 
 
 
-function getOperator(screen) {
+function getOperator() {
 const operators = document.querySelectorAll(".operators");
 
 operators.forEach(btn => {
     btn.addEventListener("click", () => {
         operator = btn.textContent;
-        screen.textContent = operator;
+        if(number1 === "") number1="0";
+        smallScreen.textContent = `${number1} ${operator}`;
         console.log("operator:", operator);
     });
 });
-
-
   return operator;
 }
 
+function getEqual(){
+    let rezultat=0;
+    let finalize = document.getElementById("equal");
+    finalize.addEventListener("click", ()=>{
+        if(number2 ==="") return;
+        switch (operator) {
+            case "+":
+                rezultat=sum(number1,number2);
+                break;
 
+            case "-":
+                rezultat=subtract(number1,number2);
+                break;
+            
+            case "×":
+                rezultat=multiply(number1,number2);
+                break;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function sum(){
-
+            case "÷":
+                rezultat=divide(number1,number2);
+                break;
+        }
+        smallScreen.textContent="";
+        screen.textContent=rezultat;
+        if(rezultat===77) screen.textContent="ŠPIKIĆ THE GOAT";
+        number1=`${rezultat}`;
+        operator="";
+        number2="";
+    })
 }
 
-function subtract(){
 
+
+
+
+
+
+
+
+function sum(a,b){
+    if(a=="." || a=="") a=0;
+    if(b=="." || b=="") b=0;
+    a=parseFloat(a);
+    b=parseFloat(b);
+
+    return a+b;
 }
 
-function multiply(){
+function subtract(a,b){
+    if(a=="." || a=="") a=0;
+    if(b=="." || b=="") b=0;
+    a=parseFloat(a);
+    b=parseFloat(b);
 
+    return a-b;
 }
 
-function divide(){
 
+function multiply(a,b){
+    if(a=="." || a=="") a=0;
+    if(b=="." || b=="") b=0;
+    a=parseFloat(a);
+    b=parseFloat(b);
+
+    return a*b;
+}
+
+function divide(a,b){
+    if(a=="." || a=="") a=0;
+    if(b=="." || b=="") b=0;
+    a=parseFloat(a);
+    b=parseFloat(b);
+
+    return a/b;
 }
 
 
+
+function deleteNumber(screen){
+    const deleteBtn = document.querySelector(".deleteBtn");
+    deleteBtn.addEventListener("click", ()=> {
+        if(number2 ==="") {
+            number1=number1.slice(0,-1);
+            screen.textContent=`${number1}`;
+        }
+            else 
+            {
+                number2=number2.slice(0,-1);
+                screen.textContent=`${number2}`;
+            }        
+        })
+}
